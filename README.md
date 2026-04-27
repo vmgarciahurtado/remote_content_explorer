047bc9c808eb312303cbc8a936c5e3c2a52152e8
Victor Garcia <victor.garcia@celuweb.com>
2026-04-26 20:17:37 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# remote_content_explorer
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/remote_content_explorer/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.remote_content_explorer
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
